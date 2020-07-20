e297ca3e2295d313c138f7fd342272d979759379
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2020-07-19 20:09:56 -0500

    added functions to get speech and log words spoken

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 const msgEl = document.getElementById('msg')
 
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
+let recognition = new window.SpeechRecognition();
+//start recognition and game
+recognition.start();
 
+//Capture user speech
+
+const onSpeak = (e) => {
+    const msg = e.results[0] [0].transcript;
+
+    // writeMessage(msg)
+    // checkNumber(msg)
+}
+
+//Generate random number
 const getRandomNumber = () => {
     return Math.floor(Math.random() * 100) +1 
 }
 
 const randomNum = getRandomNumber()
 
-console.log('Random Number:', randomNum)
+//Speak Result
+
+recognition.addEventListener('result', onSpeak)
+
